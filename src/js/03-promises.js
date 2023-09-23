27313c015183dcd6d44c1860bef8288d7b24e062
ApolinarySzumski <apolinary@poczta.onet.pl>
2023-09-23 22:54:54 +0200

    3/3 tasks done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,68 @@
+import Notiflix from 'notiflix';
+import _ from 'lodash';
+
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve(
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        )
+      );
+    } else {
+      reject(
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+      );
+    }
+  });
+  return promise;
 }
+
+const handleSubmit = e => {
+  const { delay, step, amount } = e.currentTarget.elements;
+
+  e.preventDefault();
+
+  let intervalId = null;
+
+  // //declarative variant
+  // const delayValue = Number(delay.value);
+  // const stepValue = Number(step.value);
+  // const amountValue = Number(amount.value);
+
+  // const array = _.range(1, amountValue + 1);
+  // console.log(array);
+
+  // array.forEach();
+
+  // imperative variat
+  const delayValue = Number(delay.value);
+  const stepValue = Number(step.value);
+  const amountValue = Number(amount.value);
+
+  let counter = 1;
+  let delayPlusStep = delayValue;
+
+  setTimeout(() => {
+    intervalId = setInterval(() => {
+      createPromise(counter, delayPlusStep).then(
+        onResolve => {
+          onResolve;
+        },
+        onReject => {
+          onReject;
+        }
+      );
+      counter++;
+      delayPlusStep += stepValue;
+      if (counter > amountValue) {
+        clearInterval(intervalId);
+      }
+    }, stepValue);
+  }, delayValue);
+};
+
+form.addEventListener('submit', handleSubmit);
