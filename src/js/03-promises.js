1ea2f5dbb3d906d150e6fe1d87d9b69209a988b0
ApolinarySzumski <apolinary@poczta.onet.pl>
2023-09-24 21:56:37 +0200

    second way for 3rd task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,21 +3,73 @@ import _ from 'lodash';
 
 const form = document.querySelector('.form');
 
+// // imperative variat
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   const promise = new Promise((resolve, reject) => {
+//     if (shouldResolve) {
+//       resolve(
+//         Notiflix.Notify.success(
+//           `✅ Fulfilled promise ${position} in ${delay}ms`
+//         )
+//       );
+//     } else {
+//       reject(
+//         Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+//       );
+//     }
+//   });
+//   return promise;
+// }
+
+// const handleSubmit = e => {
+//   const { delay, step, amount } = e.currentTarget.elements;
+
+//   e.preventDefault();
+
+//   let intervalId = null;
+
+//   const delayValue = Number(delay.value);
+//   const stepValue = Number(step.value);
+//   const amountValue = Number(amount.value);
+
+//   let counter = 1;
+//   let delayPlusStep = delayValue;
+
+//   setTimeout(() => {
+//     intervalId = setInterval(() => {
+//       createPromise(counter, delayPlusStep).then(
+//         onResolve => {
+//           onResolve;
+//         },
+//         onReject => {
+//           onReject;
+//         }
+//       );
+//       counter++;
+//       delayPlusStep += stepValue;
+//       if (counter > amountValue) {
+//         clearInterval(intervalId);
+//       }
+//     }, stepValue);
+//   }, delayValue);
+
+//   form.reset();
+// };
+
+// //declarative variant
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   const promise = new Promise((resolve, reject) => {
-    if (shouldResolve) {
-      resolve(
-        Notiflix.Notify.success(
-          `✅ Fulfilled promise ${position} in ${delay}ms`
-        )
-      );
-    } else {
-      reject(
-        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
-      );
-    }
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
+
   return promise;
 }
 
@@ -26,43 +78,25 @@ const handleSubmit = e => {
 
   e.preventDefault();
 
-  let intervalId = null;
-
-  // //declarative variant
-  // const delayValue = Number(delay.value);
-  // const stepValue = Number(step.value);
-  // const amountValue = Number(amount.value);
-
-  // const array = _.range(1, amountValue + 1);
-  // console.log(array);
-
-  // array.forEach();
-
-  // imperative variat
   const delayValue = Number(delay.value);
   const stepValue = Number(step.value);
   const amountValue = Number(amount.value);
 
-  let counter = 1;
-  let delayPlusStep = delayValue;
+  const amountRange = _.range(1, amountValue + 1);
 
-  setTimeout(() => {
-    intervalId = setInterval(() => {
-      createPromise(counter, delayPlusStep).then(
-        onResolve => {
-          onResolve;
-        },
-        onReject => {
-          onReject;
-        }
+  amountRange.forEach(i => {
+    const position = i;
+    const currentDelay = delayValue + stepValue * (i - 1);
+    createPromise(position, currentDelay)
+      .then(({ position, delay }) =>
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        )
+      )
+      .catch(({ position, delay }) =>
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
       );
-      counter++;
-      delayPlusStep += stepValue;
-      if (counter > amountValue) {
-        clearInterval(intervalId);
-      }
-    }, stepValue);
-  }, delayValue);
+  }, stepValue);
 };
 
 form.addEventListener('submit', handleSubmit);
